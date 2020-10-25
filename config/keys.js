02f8dc202b012acae105b3f7644161d4f0a4fff1
shimaaViola <66923459+shimaaViola@users.noreply.github.com>
2020-10-25 04:12:34 +0200

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/expense-track",
+  mongoURI: "mongodb+srv://adminAphrodite:xbRW67LuEkDd3Rg@cluster0.hocbn.mongodb.net/aphro-expenses?retryWrites=true&w=majority" || "mongodb://localhost:27017/expense-track",
   secretOrKey: process.env.SECRET || "secret",
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleSecret: process.env.GOOGLE_SECRET,
+  googleClientId: "65340800012-fr7lftvv951uqje68pbqocluks4lfqn0.apps.googleusercontent.com",
+  googleSecret: "x8Y8JUiX8GtbVgkAyfuZHIPo",
   port: process.env.PORT || 5000
 };
